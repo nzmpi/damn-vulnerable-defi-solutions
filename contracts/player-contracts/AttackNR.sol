f7cab6a61c5427063357d9220f4e939d6410cf35
nzmpi <108993233+nzmpi@users.noreply.github.com>
2023-02-22 03:27:19 +0300

    Update and rename Attack.sol to AttackNR.sol

--- contracts/player-contracts/AttackNR.sol
+++ contracts/player-contracts/AttackNR.sol
@@ -7,7 +7,7 @@ interface IPool {
     function flashLoan(IERC3156FlashBorrower receiver, address token, uint256 amount, bytes calldata data) external;
 }
 
-contract Attack {
+contract AttackNR {
     address ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     constructor (address receiver, address pool) {
